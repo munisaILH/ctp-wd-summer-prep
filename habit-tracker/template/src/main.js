c0e6afbeb4918cfeea854c8af8d16a915836cdc8
Munisa Ilhomova <152044222+munisaILH@users.noreply.github.com>
2025-08-05 20:42:49 +0000

    Week 1 HW, CSS JS and HTML changes

--- habit-tracker/template/src/main.js
+++ habit-tracker/template/src/main.js
@@ -159,7 +159,7 @@ function calculateStreak(history) {
 renderHabits();
 =======
 const form = document.getElementById('habit_form')
-const habits = [0];
+const habits = [];
 
 form.addEventListener('submit', (event) => {event.preventDefault()
     event.preventDefault()
