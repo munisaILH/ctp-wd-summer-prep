3355cd63e5de12e5242eb13ad6fa53f9db3733d6
Munisa Ilhomova <152044222+munisaILH@users.noreply.github.com>
2025-08-05 20:27:42 +0000

    Week 1 HW, Js + CSS added, HTML altered

--- habit-tracker/template/src/main.js
+++ habit-tracker/template/src/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //grrab dom elements
 const form = document.getElementById('habit-form');
 const input = document.getElementById('habit-name');
@@ -156,3 +157,28 @@ function calculateStreak(history) {
 }
 
 renderHabits();
+=======
+const form = document.getElementById('habit_form')
+const habits = [];
+
+form.addEventListener('submit', (event) => {event.preventDefault()
+    event.preventDefault()
+    const data = new FormData(event.target)     //formdata = obj
+    const habit = {
+        name: data.get('habit_name'),
+        targetStreak: Number(data.get('target_streak'))
+    }
+
+    habits.push(habit)
+
+    console.log(JSON.stringify(habits))
+    renderHabits(habits)
+})
+
+const renderHabits = (habits) => {
+    const habitList = document.getElementById('habit_list')
+    habitList.innerHTML = habits.map(habit => {
+        return `<li>${habit.name} ${habit.targetStreak}</li>`
+    }).join('\n')
+}
+>>>>>>> 8a58668 (Week 1 HW, Js + CSS added, HTML altered)
